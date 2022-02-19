01a700739af6666734310bb7e559d387a50df57b
Stephen Appleby <stephen.appleby27@gmail.com>
2022-02-19 15:06:50 +1100

    Added examples to stack

--- src/ds/stack.py
+++ src/ds/stack.py
@@ -7,6 +7,22 @@ class Stack:
 
     A last in first out (LIFO) data structure.
 
+    Example:
+
+    >>> stack = Stack(data=[x for x in range(10)])
+    >>> print(stack)
+    [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+    >>> for x in range(5):
+    ...     print(stack.pop())
+    ...
+    9
+    8
+    7
+    6
+    5
+    >>> print(stack)
+    [0, 1, 2, 3, 4]
+
     ...
 
     Attributes
